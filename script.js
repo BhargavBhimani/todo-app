8b98ec648bbd9c1a6cef9096804088fb2cfe2e65
Bhargav <bhargavbhimani55@gmail.com>
2023-09-19 11:39:00 +0530

    final commit

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', addTask);
+
+
 function addTask() {
     const taskInput = document.getElementById('task');
     const taskText = taskInput.value.trim();
     if (taskText !== '') {
         const taskList = document.getElementById('taskList');
         const taskItem = document.createElement('li');
-        taskItem.innerHTML = `<div class="space-x-12 m-2 bg-slate-100 px-2 flex flex-row items-center">
-                    <input type="checkbox" onchange="completeTask(this)">
-                    <span class="text-2xl w-[200px] text-orange-500">${taskText}</span>
-                    <button onclick="removeTask(this)" class="">X</button>
-                    </div>
-                `;
+        const div = document.createElement('div');
+        div.className = "space-x-12 m-2 bg-slate-100 px-2 flex flex-row items-center";
+
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.onchange = function () {
+            completeTask(this);
+        };
+
+        const span = document.createElement('span');
+        span.className = "text-2xl w-[200px] text-orange-500";
+        span.textContent = taskText;
+
+        const button = document.createElement('button');
+        button.textContent = "X";
+        button.onclick = function () {
+            removeTask(this);
+        };
+
+        div.appendChild(checkbox);
+        div.appendChild(span);
+        div.appendChild(button);
+        taskItem.appendChild(div);
         taskList.appendChild(taskItem);
         taskInput.value = '';
     }
@@ -25,6 +46,6 @@ function completeTask(checkbox) {
 }
 
 function removeTask(button) {
-    const taskItem = button.parentElement;
+    const taskItem = button.parentElement.parentElement;
     taskItem.remove();
 }
