84cd29391410c88bb3e913245987a93e87cff840
Bhargav <bhargavbhimani55@gmail.com>
2023-09-21 12:23:18 +0530

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const btn = document.getElementById('btn');
-btn.addEventListener('click', addTask);
+document.addEventListener('DOMContentLoaded', init);
 
+function init() {
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    });
+
+
+    const viewAll = document.getElementById('viewAll');
+    viewAll.addEventListener('click', loadTasks);
+    viewAll.addEventListener('click', function (event) {
+        if (event) {
+            viewAll.style.display = 'none'
+        }
+    })
+}
+
+const taskInput = document.getElementById('task');
+const taskList = document.getElementById('taskList');
 
 function addTask() {
-    const taskInput = document.getElementById('task');
     const taskText = taskInput.value.trim();
+
     if (taskText !== '') {
-        const taskList = document.getElementById('taskList');
         const taskItem = document.createElement('li');
         const div = document.createElement('div');
         div.className = "space-x-12 m-2 bg-slate-100 px-2 flex flex-row items-center";
@@ -33,6 +52,8 @@ function addTask() {
         taskItem.appendChild(div);
         taskList.appendChild(taskItem);
         taskInput.value = '';
+
+        saveTask(taskText);
     }
 }
 
@@ -48,4 +69,59 @@ function completeTask(checkbox) {
 function removeTask(button) {
     const taskItem = button.parentElement.parentElement;
     taskItem.remove();
+
+    const taskText = taskItem.querySelector('span').textContent;
+    removeTaskFromLocalStorage(taskText);
+}
+
+function saveTask(taskText) {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    tasks.push(taskText);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    tasks.forEach(function (taskText) {
+        const taskItem = document.createElement('li');
+        const div = document.createElement('div');
+        div.className = "space-x-12 m-2 bg-slate-100 px-2 flex flex-row items-center";
+
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.onchange = function () {
+            completeTask(this);
+        };
+
+        const span = document.createElement('span');
+        span.className = "text-2xl w-[200px] text-orange-500";
+        span.textContent = taskText;
+
+        const button = document.createElement('button');
+        button.textContent = "X";
+        button.onclick = function () {
+            removeTask(this);
+        };
+
+        div.appendChild(checkbox);
+        div.appendChild(span);
+        div.appendChild(button);
+        taskItem.appendChild(div);
+        taskList.appendChild(taskItem);
+    });
+}
+
+function removeTaskFromLocalStorage(taskText) {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+    const index = tasks.indexOf(taskText);
+    // console.log(index);
+    if (index !== -1) {
+        tasks.splice(index, 1);
+
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
 }
